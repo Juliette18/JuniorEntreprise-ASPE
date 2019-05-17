6bd24274cb38f7a800e06fcd03b1183254b74642
Juliette <juliette18.vaton@gmail.com>
2019-05-17 09:47:40 +0200

    ajout button

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ kivy.require("1.8.0")
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 class LoginScreen (GridLayout):
     def __init__(self,**kwargs):
@@ -25,11 +26,23 @@ class LoginScreen (GridLayout):
         self.add_widget(Label(text="Authentification bifactorielle:"))
         self.tfa=TextInput(multiline = False)
         self.add_widget(self.tfa)
+        self.join = Button(text="Connexion")
+        #self.join.bind(on_press=self.join_button)
+        self.add_widget(Label())
+        self.add_widget(self.join)   
         
         
         self.forgotpwd=Label(text= "Mot de passe oublié ? Envoyez un email à : jesuisundebileamnesique@je.fr")       
 
         self.add_widget(self.forgotpwd)
+        
+#        def join_button (self,instance):
+#            username = self.username.text
+#            
+#            print(username)
+            
+            
+            
 
 class Welcome (GridLayout):
     def __init__(self,**kwargs):
